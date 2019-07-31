efbb68762496e8adbf5d09f6f9107c1a99626c60
Wei Shi <shixwei@gmail.com>
2019-07-30 19:38:06 -0500

    Added google oauth, (After section 3 of the course)

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
 const app = express()
 const PORT = process.env.PORT || 5000
-app.get('/', (req, res) => {
-    res.send({hi: 'there'})
-})
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('Access token', accessToken)
+        console.log('Refresh token', refreshToken)
+        console.log('Profile', profile)
+    })
+);
+
+//Note: the 'google' here is not defined by us. It is defined within the "passport-google-oauth20" strategy.
+//      Meaning we would need to look up what string to use if other strategies are desired.
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 app.listen(PORT, () => {
     console.log('Listening on port ' + PORT)
