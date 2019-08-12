c3b069b2549bad9c28620f634c272a946af14b12
Wei Shi <shixwei@gmail.com>
2019-08-12 18:16:08 -0500

    Added billing and client side app

--- server/index.js
+++ server/index.js
@@ -28,6 +28,15 @@ require('./routes/authRoutes')(app);
 //This causes the authRoutes routes to be added to the instance of express we've defined here.
 require('./routes/billingRoutes')(app); 
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, client, build, index.html))
+    })
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log('Listening on port ' + PORT)
